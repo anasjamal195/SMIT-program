9b3e8301d2b4833cc89a2eb1c154b900f97bcac6
anasjamal195 <anasj5749@gmail.com>
2022-01-20 23:49:06 +0500

    Completed without Navbar

---
+++ Assignment-12/state-agency/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { Page_1 } from './Components/Page_1';
+import { Page_2 } from './Components/Page_2';
+import { Page_3 } from './Components/Page_3';
+import { Page_4 } from './Components/Page_4';
+import { Footer } from './Components/Footer';
+
+function App() {
+  return (
+    <div className = "container-fluid">
+      
+        <Page_1/>
+        <Page_2/>
+        <Page_3/>
+        <Page_4/>
+        
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
